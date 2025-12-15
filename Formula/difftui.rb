b339d3504e93223aa37c15d875ab6d141c1d5634
Michael Holtzscher <mholtzscher@users.noreply.github.com>
2025-12-15 00:10:19 +0000

    difftui 0.1.4

--- Formula/difftui.rb
+++ Formula/difftui.rb
@@ -1,7 +1,7 @@
 class Difftui < Formula
   desc "TUI for comparing and diffing text"
   homepage "https://github.com/mholtzscher/difftui"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
@@ -17,7 +17,7 @@ class Difftui < Formula
       sha256 "170db8eee0c7864bb5933d5d9e0de94f85a47235a58cd376730d50c45e195a62"
     end
     on_intel do
-      url "https://github.com/mholtzscher/difftui/releases/download/difftui-v0.1.3/difftui-linux-x64"
+      url "https://github.com/mholtzscher/difftui/releases/download/difftui-v0.1.4/difftui-linux-x64"
       sha256 "3ac4db133365b4b5c3efbc6886b538e11c5ca78a97bc647a5af8e074e3317884"
     end
   end
