b43b6f1c8d4f49472b10875bbab08f55b10a0ed8
Michael Holtzscher <michael@holtzscher.com>
2026-02-02 16:39:49 -0600

    chore(aerospace-utils): reorder sha256 and version fields

--- Formula/aerospace-utils.rb
+++ Formula/aerospace-utils.rb
@@ -6,8 +6,8 @@ class AerospaceUtils < Formula
   desc "CLI for managing Aerospace workspace sizing based on monitor gaps"
   homepage "https://github.com/mholtzscher/aerospace-utils"
   url "https://github.com/mholtzscher/aerospace-utils/releases/download/v0.3.3/aerospace-utils_0.3.3_darwin_arm64.tar.gz"
-  sha256 "38b2fb4b95f5035e5ee7caf9bed0fbc81dc01f32666c641daed8f9ff5eceb956"
   version "0.3.3"
+  sha256 "38b2fb4b95f5035e5ee7caf9bed0fbc81dc01f32666c641daed8f9ff5eceb956"
   license "MIT"
   depends_on arch: :arm64
   depends_on :macos
